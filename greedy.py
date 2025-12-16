601dc4ed93885e6847d966bf7f4aa5c9cddb20e7
JayReeMarine <lee010518@gmail.com>
2025-12-16 11:55:05 +1100

    apply greedy algorithm before testing

--- greedy.py
+++ greedy.py
@@ -31,5 +31,55 @@ def compute_energy_saving(cp:int, dp: int) -> float:
     """simple version of energy saving calculation"""
     return dp - cp
 
+def greedy_platoon_matching(avs: ActiveVehicle, pvs: PassiveVehicle, l_min: int) -> Tuple[List[Assignment], float]:
+    """
+    Greedy Maximum-weight platoon Matching
+
+    Returns values:
+        - assignments: List of Assignment (pv, av, cp, dp)
+        - total energy saving: sum(dp - cp)
+    """
+
+    # 1. initialising candidate set
+    candidates: List[Tuple[int, ActiveVehicle, PassiveVehicle, int, int]] = []
+    # tuple: (saving, av, pv, cp, dp)
+
+    for av in avs:
+        for pv in pvs:
+            shared = compute_shared_segment(av, pv)
+            if shared is None:
+                continue
+            cp, dp = shared
+            if (dp- cp) < l_min:
+                continue
+            
+            saving = compute_energy_saving(cp, dp)
+            candidates.append((saving, av, pv, cp, dp))
+    
+    # 2. sort candidates by energy saving (descending order)
+    candidates.sort(ket=lambda x: x[0], reverse=True)
+
+    # 3. Assign based on sort order
+    assignments = List[Assignment] = []
+    used_pvs: Set[str] = set()
+    av_load: Dict[str, int] = {av.id:0 for av in avs}
+
+    total_saving = 0.0
+
+    for saving, av, pv, cp, dp in candidates:
+        #check condition before loading
+        if pv.id in used_pvs:
+            continue
+        if av_load[av.id] >= av.capacity:
+            continue
+        
+        #assign
+        assignments.append(Assignment(pv, av, cp, dp))
+        used_pvs.add(pv.id)
+        av_load[av.id] += 1
+        total_saving += saving
+    
+    return assignments, total_saving
+
 
 
